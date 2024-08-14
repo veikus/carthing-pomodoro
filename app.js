c7aa9ee5846a10661044c97ec3f8b9360137ac0b
Artem Veikus <artem@veikus.com>
2024-08-14 20:27:44 +0200

    Initial version

--- app.js
+++ app.js
@@ -1,51 +1,158 @@
 (() => {
-  // const root = document.getElementById('root');
-  // root.innerHTML = '<p>Press any key 2</p><a onclick="window.location.reload()">Reload 2</a>';
-  // document.addEventListener('keydown', (e) => {
-  //   root.innerHTML = `<p>${e.key} ${e.keyCode}</p><a onclick="window.location.reload()">Reload 2</a>`;
-  // });
-  //
-  // document.addEventListener('wheel', (e) => {
-  //   console.log(e);
-  //   root.innerHTML = `scrolling... ${e.deltaX} ${e.deltaY}`;
-  // });
+  const CONFIG_BUTTON = 'Enter';
+  const SKIP_BUTTON = 'Escape';
+  const RESET_BUTTON = '1';
 
+  let DEFAULT_FOCUS_TIME = parseInt(localStorage.getItem('focusTime'), 10) || 20 * 60;
+  let DEFAULT_RELAX_TIME = parseInt(localStorage.getItem('relaxTime'), 10) || 5 * 60;
+  let collectedPomodoros = parseInt(localStorage.getItem('collectedPomodoros'), 10) || 0;
+
+  let isConfiguring = false;
+  let currentState = 'focus'; // focus, relax
+  let isTimerRunning = false;
+  let timeLeft = DEFAULT_FOCUS_TIME;
+
+  const configModeLabel = document.getElementById('config-mode-label');
   const timerDisplay = document.getElementById('timer');
-  const startBtn = document.getElementById('start-btn');
-  const resetBtn = document.getElementById('reset-btn');
-
-  let timeLeft = 25 * 60; // 25 минут в секундах
-  let timerInterval;
 
   function updateTimerDisplay() {
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = timeLeft % 60;
+    let value = timeLeft;
+    if (isConfiguring) {
+      if (currentState === 'focus') {
+        value = DEFAULT_FOCUS_TIME;
+      } else {
+        value = DEFAULT_RELAX_TIME
+      }
+    }
+
+    const minutes = Math.floor(value / 60);
+    const seconds = value % 60;
     timerDisplay.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
   }
 
-  function startTimer() {
-    if (!timerInterval) {
-      timerInterval = setInterval(() => {
-        if (timeLeft > 0) {
-          timeLeft--;
-          updateTimerDisplay();
-        } else {
-          clearInterval(timerInterval);
-          alert('Time is up! Take a break.');
-        }
-      }, 1000);
+  function updatePomodorosDisplay() {
+    let str = '';
+    for (let i = 0; i < collectedPomodoros; i++) {
+      str += '🍅';
     }
+
+    const pomodorosDisplay = document.getElementById('pomodoros');
+    pomodorosDisplay.textContent = str || 'Collect your first pomodoro!'
   }
 
-  function resetTimer() {
-    clearInterval(timerInterval);
-    timerInterval = null;
-    timeLeft = 25 * 60;
+  function toggleState() {
+    const body = document.querySelector('body');
+
+    switch (currentState) {
+      case 'focus':
+        timeLeft = DEFAULT_RELAX_TIME;
+        currentState = 'relax';
+        body.className = 'relaxMode';
+        break;
+
+      case 'relax':
+        timeLeft = DEFAULT_FOCUS_TIME;
+        currentState = 'focus';
+        body.className = 'focusMode';
+        break;
+    }
+
+    isTimerRunning = false;
     updateTimerDisplay();
   }
 
-  startBtn.addEventListener('click', startTimer);
-  resetBtn.addEventListener('click', resetTimer);
+  function tick() {
+    if (isTimerRunning) {
+      timeLeft--;
+
+      if (timeLeft === 0 && currentState === 'focus') {
+        collectedPomodoros++;
+        updatePomodorosDisplay();
+        localStorage.setItem('collectedPomodoros', collectedPomodoros);
+      }
+
+      if (timeLeft === 0) {
+        isTimerRunning = false;
+        toggleState();
+      }
+
+      updateTimerDisplay();
+    }
+  }
 
   updateTimerDisplay();
+  updatePomodorosDisplay();
+  setInterval(() => {
+    tick();
+  }, 1000);
+
+  document.addEventListener('click', (e) => {
+    if (isConfiguring) {
+      return;
+    }
+
+    isTimerRunning = !isTimerRunning;
+    updateTimerDisplay();
+  });
+
+  document.addEventListener('keydown', (e) => {
+    switch (e.key) {
+      case CONFIG_BUTTON:
+        if (isTimerRunning) {
+          const confirmed = confirm('This will stop the timer. Are you sure?');
+          if (!confirmed) {
+            return;
+          }
+        }
+
+        isTimerRunning = false;
+        isConfiguring = !isConfiguring;
+        configModeLabel.style.display = isConfiguring ? 'block' : 'none';
+        break;
+
+      case SKIP_BUTTON:
+        if (isTimerRunning) {
+          const confirmed = confirm('This will stop the timer. Are you sure?');
+          if (!confirmed) {
+            return;
+          }
+        }
+
+        toggleState();
+        break;
+
+      case RESET_BUTTON:
+        collectedPomodoros = 0;
+        updatePomodorosDisplay();
+        localStorage.setItem('collectedPomodoros', collectedPomodoros);
+        break;
+    }
+
+    updateTimerDisplay();
+  });
+
+  document.addEventListener('wheel', (e) => {
+    if (!isConfiguring) {
+      return;
+    }
+
+    const isUp = e.deltaX > 0;
+    switch (currentState) {
+      case 'focus':
+        DEFAULT_FOCUS_TIME = isUp ? DEFAULT_FOCUS_TIME + 30 : DEFAULT_FOCUS_TIME - 30;
+        DEFAULT_FOCUS_TIME = Math.max(30, DEFAULT_FOCUS_TIME);
+        timeLeft = DEFAULT_FOCUS_TIME;
+        localStorage.setItem('focusTime', DEFAULT_FOCUS_TIME);
+        break;
+
+      case 'relax':
+        DEFAULT_RELAX_TIME = isUp ? DEFAULT_RELAX_TIME + 30 : DEFAULT_RELAX_TIME - 30;
+        DEFAULT_RELAX_TIME = Math.max(30, DEFAULT_RELAX_TIME);
+        timeLeft = DEFAULT_RELAX_TIME;
+        localStorage.setItem('relaxTime', DEFAULT_RELAX_TIME);
+        break;
+    }
+
+    updateTimerDisplay();
+  });
 })();
