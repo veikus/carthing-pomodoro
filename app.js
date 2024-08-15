eee099cc815b16ee9262d678e6886228223735d5
Artem Veikus <artem@veikus.com>
2024-08-15 20:35:50 +0200

    Add summary of focus/relax time mode

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 (() => {
   const START_BUTTON = 'Enter'; // Works same way as clicking on the screen
   const SKIP_BUTTON = 'Escape';
-  const RESET_BUTTON = '1';
+  const RESET_BUTTON = 'm';
+  const MODE_BUTTON = '1';
 
   let DEFAULT_FOCUS_TIME = parseInt(localStorage.getItem('focusTime'), 10) || 20 * 60;
   let DEFAULT_RELAX_TIME = parseInt(localStorage.getItem('relaxTime'), 10) || 5 * 60;
+  let viewMode = localStorage.getItem('viewMode') || 'pomodoros'; // pomodoros, totalTime
   let collectedPomodoros = parseInt(localStorage.getItem('collectedPomodoros'), 10) || 0;
+  let totalFocusTime = parseInt(localStorage.getItem('totalFocusTime'), 10) || 0;
+  let totalRelaxTime = parseInt(localStorage.getItem('totalRelaxTime'), 10) || 0;
 
   let currentState = 'focus'; // focus, relax
   let isTimerStarted = false;
@@ -35,6 +39,10 @@
       case RESET_BUTTON:
         reset();
         break;
+
+      case MODE_BUTTON:
+        toggleViewMode();
+        break;
     }
   });
 
@@ -44,24 +52,44 @@
   });
 
   function updateTimerDisplay() {
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = timeLeft % 60;
-    timerDisplay.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    timerDisplay.textContent = formatTime(timeLeft);
   }
 
   function updatePomodorosDisplay() {
-    let str = '';
-    for (let i = 0; i < collectedPomodoros; i++) {
-      str += '🍅';
-    }
-
     const pomodorosDisplay = document.getElementById('pomodoros');
-    pomodorosDisplay.textContent = str || 'Collect your first pomodoro!'
+
+    switch (viewMode) {
+      case 'pomodoros': {
+        let str = '';
+        for (let i = 0; i < collectedPomodoros; i++) {
+          str += '🍅';
+        }
+
+        pomodorosDisplay.textContent = str || 'Collect your first pomodoro!';
+        break;
+      }
+
+      case 'totalTime': {
+        pomodorosDisplay.textContent =  `${formatTime(totalFocusTime)} | ${formatTime(totalRelaxTime)}`;
+        break;
+      }
+    }
   }
 
   function tick() {
     if (isTimerRunning) {
       timeLeft--;
+
+      switch (currentState) {
+        case 'focus':
+          totalFocusTime++;
+          localStorage.setItem('totalFocusTime', totalFocusTime.toString());
+          break;
+
+        case 'relax':
+          totalRelaxTime++;
+          localStorage.setItem('totalRelaxTime', totalRelaxTime.toString());
+      }
 
       if (timeLeft === 0 && currentState === 'focus') {
         collectedPomodoros++;
@@ -76,6 +104,9 @@
       }
 
       updateTimerDisplay();
+      if (viewMode === 'totalTime') {
+        updatePomodorosDisplay();
+      }
     }
   }
 
@@ -101,6 +132,12 @@
     updateTimerDisplay();
   }
 
+  function toggleViewMode() {
+    viewMode = viewMode === 'pomodoros' ? 'totalTime' : 'pomodoros';
+    localStorage.setItem('viewMode', viewMode);
+    updatePomodorosDisplay();
+  }
+
   function start() {
     isTimerStarted = true;
     isTimerRunning = !isTimerRunning;
@@ -117,8 +154,18 @@
 
   function reset() {
     collectedPomodoros = 0;
+    totalFocusTime = 0;
+    totalRelaxTime = 0;
     updatePomodorosDisplay();
     localStorage.setItem('collectedPomodoros', collectedPomodoros);
+    localStorage.setItem('totalFocusTime', totalFocusTime);
+    localStorage.setItem('totalRelaxTime', totalRelaxTime);
+  }
+
+  function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
   }
 
   function changeTimeSettings(isUp) {
